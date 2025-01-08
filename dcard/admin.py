a9e50707d7a76cf9ebfe91297df88bf31e1d73c8
11156032 <11156032@ntub.edu.tw>
2025-01-08 17:35:42 +0800

    個人圖片

--- dcard/admin.py
+++ dcard/admin.py
@@ -1,8 +1,8 @@
-from dcard.models import Post
+from dcard.models import Post, UserProfile
 from django.contrib import admin
-
 
 class DcardAdmin(admin.ModelAdmin):
     list_display = ('category', 'title', 'author', 'pub_date')
     
 admin.site.register(Post, DcardAdmin)
+admin.site.register(UserProfile)
